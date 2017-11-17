e2567a60ec99e2c88775485d0379273ca3aabf4f
Mike Walmsley <walmsleymk1@gmail.com>
2017-11-17 06:04:29 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,26 @@
-from setuptools import find_packages, setup
+# from setuptools import find_packages, setup
 
 
-setup(
-    name='whoseline-vis',
-    version='0.1.0',
-    url='https://github.com/bmorris3/whoseline-vis',
-    license='BSD',
-    author='Whose Line Hack Group',
-    author_email='',
-    description='',
-    include_package_data=True,
-    packages=find_packages(),
-    install_requires=[
-        "astropy",
-        "bokeh",
-        "Flask",
-        "Jinja2",
-        "numpy",
-        "pandas",
-        "six"
-    ],
-    entry_points = {
-        'console_scripts': ['wla=whoseline_vis.__init__:start'],
-    }
-)
+# setup(
+#     name='whoseline-vis',
+#     version='0.1.0',
+#     url='https://github.com/bmorris3/whoseline-vis',
+#     license='BSD',
+#     author='Whose Line Hack Group',
+#     author_email='',
+#     description='',
+#     include_package_data=True,
+#     packages=find_packages(),
+#     install_requires=[
+#         "astropy",
+#         "bokeh",
+#         "Flask",
+#         "Jinja2",
+#         "numpy",
+#         "pandas",
+#         "six"
+#     ],
+#     entry_points = {
+#         'console_scripts': ['wla=whoseline_vis.__init__:start'],
+#     }
+# )
